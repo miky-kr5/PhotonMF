54979c38180a84361cb262fb5dab6b8b43e6ed0b
Miguel Angel Astor Romero <miguel.astor@ciens.ucv.ve>
2017-01-12 17:29:18 -0400

    Made some camera attributes private.

--- camera.hpp
+++ camera.hpp
@@ -16,21 +16,17 @@ using glm::inverse;
 
 class Camera {
 public:
-  int m_h;
-  int m_w;
-  float m_fov;
-  float m_a_ratio;
   vec3 m_eye;
   vec3 m_look;
   vec3 m_up;
 
   Camera(int h = 480, int w = 640, float fov = 90.0f, vec3 _e = vec3(0.0f), vec3 _l = vec3(0.0f, 0.0f, -1.0f), vec3 _u = vec3(0.0f, 1.0f, 0.0f)):
-    m_h(h),
-    m_w(w),
-    m_fov(fov),
     m_eye(_e),
     m_look(_l),
-    m_up(normalize(_u))
+    m_up(normalize(_u)),
+    m_h(h),
+    m_w(w),
+    m_fov(fov)
   {
     m_a_ratio = static_cast<float>(w) / static_cast<float>(h);
     m_inv_view_matrix = inverse(lookAt(_e, _l, _u));
@@ -45,6 +41,10 @@ public:
   void view_to_world(Ray & r) const;
 
 private:
+  int m_h;
+  int m_w;
+  float m_fov;
+  float m_a_ratio;
   mat4 m_inv_view_matrix;
 };
 
